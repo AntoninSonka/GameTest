be65398cd96e43e3f5433aec9aae31807c4bf4c2
AntoninSonka <antonin.sonka@outlook.com>
2023-10-13 18:50:44 +0200

    promazani

--- src/main.cpp
+++ src/main.cpp
@@ -16,20 +16,6 @@ int main(){
 
     sf::RenderWindow window(sf::VideoMode(1024, 1024.f / 1.5f), "Game Test");
 
-    /*sf::Texture texture[9];
-
-    sf::Vector2i gridSize;
-
-    std::vector<std::string> sGrid;
-    std::vector<std::string> sEGrid;
-    std::vector<std::string> sFGrid;
-
-    std::vector<std::vector<BackgroundTile>> grid;
-    std::vector<std::vector<EntityTile>> eGrid;
-    std::vector<std::vector<EffectTile>> fGrid;
-
-    int currentMap = 1;
-    */
     sf::Texture texture[9];
 
     setupMap(mapData.currentMap, texture);
@@ -38,17 +24,12 @@ int main(){
 
     window.setFramerateLimit(60); 
 
-    //sf::Vector2i playerPos = sf::Vector2i(1, 1);
 
     sf::View view(sf::Vector2f(playerData.playerPos.x * 16 + 8, playerData.playerPos.y * 16 + 8), sf::Vector2f(240.f, 240.f / 1.5f));
 
 
-    //sf::Vector2i lastTile, currentTile;
     bool isThere = true;
     bool sprint = false;
-
-    //lastTile = sf::Vector2i(14 + playerPos.x, 10 + playerPos.y);
-    //currentTile = sf::Vector2i(14 + playerPos.x, 10 + playerPos.y);
 
     setPlayerProps(&texture[2], &texture[5], &texture[6], &texture[7], &texture[8]);
  
